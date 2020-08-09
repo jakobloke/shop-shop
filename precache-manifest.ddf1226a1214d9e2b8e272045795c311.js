6872957f38200035626bcbe352101e15703eea1d
Jakob <61665831+jakobloke@users.noreply.github.com>
2020-08-09 16:27:09 -0700

    Updates

---
+++ precache-manifest.ddf1226a1214d9e2b8e272045795c311.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0df70836f0173718c0041155234356ed",
+    "url": "/shop-shop/index.html"
+  },
+  {
+    "revision": "4ffcc9f02c3790b227e6",
+    "url": "/shop-shop/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/shop-shop/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shop-shop/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ffcc9f02c3790b227e6",
+    "url": "/shop-shop/static/js/main.a4fc650b.chunk.js"
+  },
+  {
+    "revision": "a81e7981bdcca8e5f4fe",
+    "url": "/shop-shop/static/js/runtime-main.71b1c067.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shop-shop/static/media/logo.5d5d9eef.svg"
+  }
+]);
