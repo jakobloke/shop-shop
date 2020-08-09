92496f7ce882a9499b66dc8811f306927d020057
Jakob <61665831+jakobloke@users.noreply.github.com>
2020-08-09 16:02:35 -0700

    final updates

---
+++ client/build/precache-manifest.1534fc1d6ee7b49439da0afc5f4ac899.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbb109f232a13f204652f41d580c3281",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d4937261029b2729508",
+    "url": "/static/css/main.4f63fd05.chunk.css"
+  },
+  {
+    "revision": "feab48bcdf20bf029b4f",
+    "url": "/static/js/2.61261990.chunk.js"
+  },
+  {
+    "revision": "700f66a07a195ca4626c39af7f9fa9f3",
+    "url": "/static/js/2.61261990.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d4937261029b2729508",
+    "url": "/static/js/main.7c63a7e8.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
